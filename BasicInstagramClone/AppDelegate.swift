cc92a1c10962fccde4754cf54543f531f4c20789
IremTektas <tektasirem13@gmail.com>
2023-03-05 22:06:24 +0300

    one signal extension configuration

--- BasicInstagramClone/AppDelegate.swift
+++ BasicInstagramClone/AppDelegate.swift
@@ -10,6 +10,7 @@ import CoreData
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import OneSignal
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // Remove this method to stop OneSignal Debugging
+          OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+          
+          // OneSignal initialization
+          OneSignal.initWithLaunchOptions(launchOptions)
+          OneSignal.setAppId("YOUR_ONESIGNAL_APP_ID")
+          
+          // promptForPushNotifications will show the native iOS notification permission prompt.
+          // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+          OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+          })
+          
+          // Set your customer userId
+          // OneSignal.setExternalUserId("userId")
         
         return true
     }
